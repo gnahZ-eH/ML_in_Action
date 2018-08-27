eb96898ea03ad3952867bbbc5bfc2b24d35ad9d1
He Zhang (Joe) <zhanghe5@corp.netease.com>
2018-08-28 01:06:00 +0800

    kNN add datingTestSet

---
+++ kNN/kNN.py
@@ -0,0 +1,61 @@
+from numpy import *
+import operator
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+def creatDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+
+def classfy0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistance = sqDiffMat.sum(axis=1)
+    distances = sqDistance ** 0.5
+    sortedDistIndices = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndices[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+# group, labels = creatDataSet()
+
+# print classfy0([0,0], group, labels, 3)
+
+
+def file2matrix(fileName):
+    fr = open(fileName)
+    arrayOlines = fr.readlines()
+    numberOfLines = len(arrayOlines)
+    resultMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOlines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        resultMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return resultMat, classLabelVector
+
+
+
+mat, lab = file2matrix('datingTestSet.txt')
+
+print mat
+print lab[0:20]
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(mat[:, 0], mat[:, 1], 15.0*array(lab), 15.0*array(lab))
+plt.show()
